3b58781f515c886bfb6e8466be40bbd2741d151a
immu27 <immusri27@gmail.com>
2026-03-03 12:48:09 +0530

    UC6: Refactored banner logic into static helper methods

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,15 +1,32 @@
 /**
  * OOPSBannerApp
- * UC5: Render OOPS as Banner using Inline Array Initialization
+ * UC6: Refactor Banner Logic into Static Functions
  *
  * @author Immanuel
- * @version 5.0
+ * @version 6.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] o = {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ", o[i], o[i], p[i], s[i]);
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    // Helper method for O
+    public static String[] getOPattern() {
+        return new String[]{
                 "  *****  ",
                 " *     * ",
                 " *     * ",
@@ -18,8 +35,11 @@ public class OOPSBannerApp {
                 " *     * ",
                 "  *****  "
         };
+    }
 
-        String[] p = {
+    // Helper method for P
+    public static String[] getPPattern() {
+        return new String[]{
                 " ******  ",
                 " *     * ",
                 " *     * ",
@@ -28,8 +48,11 @@ public class OOPSBannerApp {
                 " *       ",
                 " *       "
         };
+    }
 
-        String[] s = {
+    // Helper method for S
+    public static String[] getSPattern() {
+        return new String[]{
                 "  *****  ",
                 " *     * ",
                 " *       ",
@@ -38,21 +61,5 @@ public class OOPSBannerApp {
                 " *     * ",
                 "  *****  "
         };
-
-        // Inline declaration + initialization
-        String[] banner = {
-                String.join(" ", o[0], o[0], p[0], s[0]),
-                String.join(" ", o[1], o[1], p[1], s[1]),
-                String.join(" ", o[2], o[2], p[2], s[2]),
-                String.join(" ", o[3], o[3], p[3], s[3]),
-                String.join(" ", o[4], o[4], p[4], s[4]),
-                String.join(" ", o[5], o[5], p[5], s[5]),
-                String.join(" ", o[6], o[6], p[6], s[6])
-        };
-
-        // Enhanced for-loop
-        for (String line : banner) {
-            System.out.println(line);
-        }
     }
 }
